487250efcd7250b9ee3566d37df8b013a9a671fd
krab7191 <karstenrab91@gmail.com>
2014-07-07 14:53:19 -0400

    Sync

---
+++ public_html/public_html/calendar/events.json.php
@@ -0,0 +1,53 @@
+{
+	"success": 1,
+	"result": [
+		{
+			"id": "293",
+			"title": "Sports",
+			"url": "http://www.example.com/",
+			"class": "event-warning",
+			"start": "1362938400000",
+			"end":   "1363197686300"
+		},
+		{
+			"id": "256",
+			"title": "Food & Beverage",
+			"url": "http://www.example.com/",
+			"class": "event-success",
+			"start": "1363155300000",
+			"end":   "1363227600000"
+		},
+		{
+			"id": "276",
+			"title": "Recreational Activities",
+			"url": "http://www.example.com/",
+			"class": "event-important",
+			"start": "1363245600000",
+			"end":   "1363252200000"
+		},
+		{
+			"id": "294",
+			"title": "Seasonal",
+			"url": "http://www.example.com/",
+			"class": "event-info",
+			"start": "1363111200000",
+			"end":   "1363284086400"
+		},
+		{
+			"id": "297",
+			"title": "Entertainment",
+			"url": "http://www.example.com/",
+			"class": "event-inverse",
+			"start": "1363234500000",
+			"end":   "1363284062400"
+		},
+		{
+			"id": "54",
+			"title": "Arts, Crafts, & Hobbies",
+			"url": "http://www.example.com/",
+			"class": "event-special",
+			"start": "1363712400000",
+			"end":   "1363716086400"
+		}
+	]
+}
