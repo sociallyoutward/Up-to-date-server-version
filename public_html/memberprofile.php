e9c55b96c80ae4c1a9ecf0579a759f4fbd17c906
Arie <awprogramming@gmail.com>
2014-09-15 15:51:56 -0400

    git ignore

--- public_html/memberprofile.php
+++ public_html/memberprofile.php
@@ -1,43 +1,15 @@
 <?php
-if(!isset($_COOKIE['user']))
-    {
-	header('location: index.php');
-    }
-session_start();
-
-
-if(!isset($_SESSION['fb_token']))
-{
-	include 'reauthenticate.php';
-}
+require 'permissionChecks.php';
 require 'getLogout.php';
 ?>
 <html>
 <head>
-    <title>My Profile</title>
+    <title>My Profile</title> <!-- ADD TITLE -->
     
-    <!-- Bootstrap 3.1.1. Latest compiled and minified CSS -->
-    <link rel='stylesheet' href='http://netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css'>
+    <?php readfile('linkedfiles.php') ?> <!-- STANDARD LINKED FILES HERE -->
     
-    <!-- JQuery -->
-    <script src='http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js'></script>
     
-    <!-- Bootstrap 3.1.1 Latest compiled and minified JavaScript -->
-    <script src='http://netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js'></script>
-    
-    <!-- Socially Outward Styles -->
-    <link href='css/memberProfile.css' type='text/css' rel='stylesheet'>
-    <link href='css/navigationTemplate.css' type='text/css' rel='stylesheet'>
-    <link href='css/styles.css' type='text/css' rel='stylesheet'>
-    <link href='css/webPosition.css' type='text/css' rel='stylesheet'>
-    <link href="css/boxTransition.css" type="text/css" rel="stylesheet"/>
-    <link href="css/SO-status.css" type="text/css" rel="stylesheet"/>
-    <link href="css/activityFeed.css" type="text/css" rel="stylesheet"/>
-    <link href="css/activitySidebar.css" type="text/css" rel="stylesheet">
-    <link type="text/css" href="css/bulletinBoxStyle.css" rel="stylesheet" />
-    
-    <link rel='icon' href='assets/logo.png'>
-    
+    <!-- LINKED FILES FOR THIS PAGE START -->
     <script src='http://code.createjs.com/createjs-2013.12.12.min.js'></script>
     <script src='http://code.createjs.com/createjs-2013.02.12.min.js'></script>
     
@@ -46,102 +18,19 @@ require 'getLogout.php';
     <script src='js/bubbles/dbbubble.js'></script>
     <script type="text/javascript" src="js/ajaxCacher.js"></script>
     <script src='js/clearInterests.js'></script>
-    <script type='text/javascript' src='js/showMenu.js'></script>
-    <script src='js/sideMenu.js'></script>
-    <script type="text/javascript" src="js/slideBulletinMenu.js"></script>
-    <script src="js/modernizr.custom.js"></script>
+    <!-- LINKED FILES FOR THIS PAGE END -->
     
-    <!--Size document to browser viewport-->
-    <script src="js/windowDimensions.js" type="text/javascript"></script>
 </head>
 <?php
 // flush the buffer
 flush();
 ?>
-
 <body>
-    <div class='container'>
-	<nav class='navbar navbar-default navbar-fixed-top' role='navigation'>
-	    <div class='container-fluid'>
-	      <!-- Brand and toggle get grouped for better mobile display -->
-	      <div class='navbar-header'>
-		<button type='button' class='navbar-toggle' data-toggle='collapse' data-target='#bs-example-navbar-collapse-1'>
-		  <span class='sr-only'>Toggle navigation</span>
-		  <span><img src='assets/toggle_down.png' height='15px'</span>
-		</button>
-		<a class='navbar-brand' href='memberprofile.php'><img src='assets/brand.png' height='60px' /></a>
-	      </div>
-		
-	      <!-- Collect the nav links, forms, and other content for toggling -->
-	      <div class='collapse navbar-collapse' id='bs-example-navbar-collapse-1'>
-		<ul class='nav navbar-nav'>
-		  <li><a href='calendar/events.php'>Events</a></li>
-		  <li><a href='neighbors.php'>Neighbors</a></li>
-		  <li><a href='community.php'>Community</a></li>
-		  <li>
-		    <div id="bulletinBox">
-		  		<div id="topBox">
-			    	<p id="appendTarget"style="color:white;">Bulletin Board <img src="../assets/right_arrow.png" height="12px" width="12px"></p>
-			    </div>	
-			    <div id="bottomBox">
-			        <p id="firstOne"><a href="../feed/bulletin.php">&nbsp;Updates</a></p>
-			        <p id="secondOne"><a href="../feed/bulletin.php">Promotions</a></p>
-			        <p id="thirdOne"><a href="../feed/bulletin.php">Events</a></p>
-			    </div>
-		    </div>
-		  </li>
-		</ul>
-		<p class='navbar-text navbar-right hidden-sm hidden-xs'>explore your community</p>
-	      </div><!-- /.navbar-collapse -->
-	    </div><!-- /.container-fluid -->
-	</nav>
+
 	
-	<div class='row'>
+	<?php include 'nav.php' ?> <!-- NAV HERE -->
 	    
-	    <!--Absolute Position on Md/Lg Screen-->
-	    <div id='absolute' class='hidden-xs'>
-		<div class='col-xs=2'>
-		    <img src='assets/toggle_empty.png' height='30px'/>
-		</div>
-		<div class='col-xs-10'>
-		    <div id='sideNav side-Navigation' class='sNav'>
-			<div id='sNav-inner'>
-			<p id='name' class='pushover'><?php echo $_SESSION["fb_name"]; ?></p>
-			<img id='profpic' class='spaceUnder pushover' src='https://graph.facebook.com/<?php echo $_SESSION["fb_id"]; ?>/picture?height=350&width=350'>
-			<ul class='po'>
-			    <li class='spaceUnder'><a href='memberprofile.php'>Home</a></li>
-			    <li class='spaceUnder'><a href='messages.php'>Messages</a></li>
-			    <li class='spaceUnder'><a href='settings.php'>Settings</a></li>
-			    <li class='spaceUnder'><a href='chooseInterests.php'>Choose Interests</a></li>
-			    <li class='spaceUnder'><a href='<?php echo $logoutUrl?>'>Logout</a></li>
-			</ul>
-		    </div>
-		    </div>
-		</div>
-	    </div><!-- end toggleSide -->
-	    
-	    <!--Toggle Nav on Sm/Xs Screen-->
-	    <div id='toggleSide' class='hidden-sm hidden-md hidden-lg'>
-		<div class='col-xs=2'>
-		    <img src='assets/toggle_right.png' height='30px'/>
-		</div>
-		<div class='col-xs-10'>
-		    <div id='sideNav side-Navigation' class='sNav'>
-			<div id='sNav-inner'>
-			<p id='name' class='pushover'><?php echo $_SESSION["fb_name"]; ?></p>
-			<img id='profpic' class='spaceUnder pushover' src='https://graph.facebook.com/<?php echo $_SESSION["fb_id"]; ?>/picture?height=350&width=350'>
-			<ul class='po'>
-			    <li class='spaceUnder'><a href='memberprofile.php'>Home</a></li>
-			    <li class='spaceUnder'><a href='messages.php'>Messages</a></li>
-			    <li class='spaceUnder'><a href='settings.php'>Settings</a></li>
-			    <li class='spaceUnder'><a href='chooseInterests.php'>Choose Interests</a></li>
-			    <li class='spaceUnder'><a href='<?php echo $logoutUrl; ?>'>Logout</a></li>
-			</ul>
-		    </div>
-		    </div>
-		</div>
-	    </div><!-- end toggleSide -->
-	    
+	    <!-- THIS PAGE CONTENT START HERE -->
 	    <div id='content' class='row'>
 		<div id="bl-main" class="bl-main">
 		    <section>
@@ -264,13 +153,16 @@ flush();
 		    </section>
 		</div>
 	    </div> <!-- end #content -->
+
+	    <!-- THIS PAGE CONTENT END HERE -->
 	    
 	    <div id='user' hidden='false'><?php  print_r($_COOKIE['user']); ?></div>
 	    
+	<!-- FOR CLOSING OFF NAVIGATION -->
 	</div><!-- end .row -->
     </div><!-- end .container-->
+    <!-- END CLOSING OFF NAVIGATION -->
 
-    
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
     <script src="js/boxTransition.js"></script>
     <script>
