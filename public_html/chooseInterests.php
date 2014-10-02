083f9eb0ab8a837b3ca0212540e66524b85a50ca
Arie <awprogramming@gmail.com>
2014-10-02 14:03:08 -0400

    Added template to chooseInterests page

--- public_html/chooseInterests.php
+++ public_html/chooseInterests.php
@@ -1,36 +1,13 @@
 <?php
-if(!isset($_COOKIE['user']))
-{
-    header('location: index.php');
-}
-
-require 'fbconfig.php';
+require 'permissionChecks.php';
+require 'getLogout.php';
 ?>
 <html>
 <head>
     <title>Choose Interests</title>
     
-    <!-- Bootstrap 3.1.1. Latest compiled and minified CSS -->
-    <link rel="stylesheet" href="http://netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
-    
-    <!-- JQuery -->
-    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
-    
-    <!-- Bootstrap 3.1.1 Latest compiled and minified JavaScript -->
-    <script src="http://netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
-    
-    <!-- Socially Outward Styles -->
-    <link href="css/memberProfile.css" type="text/css" rel="stylesheet">
-    <link href="css/navigationTemplate.css" type="text/css" rel="stylesheet">
-    <link href="css/styles.css" type="text/css" rel="stylesheet">
-    <link href='css/webPosition.css' type='text/css' rel='stylesheet'>
-    <link type="text/css" href="css/bulletinBoxStyle.css" rel="stylesheet" />
-    
-    <script src=" http://code.createjs.com/createjs-2013.02.12.min.js"></script>
-    <script src="http://code.jquery.com/jquery-1.9.1.js"></script>
-    <script type="text/javascript" src='js/showMenu.js'></script>
+    <?php readfile('linkedfiles.php') ?>
     <script src="http://code.createjs.com/createjs-2013.02.12.min.js"></script>
-    <script src="http://code.jquery.com/jquery-1.9.1.js"></script>
     <script src="js/bubbles/dbinitialize2.js"></script>
     <script src="js/bubbles/resize.js"></script>
     <script src="js/bubbles/dbbubble.js"></script>
@@ -38,69 +15,14 @@ require 'fbconfig.php';
     <script src="js/sideMenu.js"></script>
     <script type="text/javascript" src="js/slideBulletinMenu.js"></script>
     <script type="text/javascript" src="js/ajaxCacher.js"></script>
-    
-    <!--Size document to browser viewport-->
-    <script src="js/windowDimensions.js" type="text/javascript"></script>
-</head>
 
+</head>
+<?php
+// flush the buffer
+flush();
+?>
 <body>
-    <div class="container">
-	<nav class="navbar navbar-default navbar-fixed-top" role="navigation">
-	    <div class="container-fluid">
-	      <!-- Brand and toggle get grouped for better mobile display -->
-	      <div class="navbar-header">
-		<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-		  <span class="sr-only">Toggle navigation</span>
-		  <span><img src="assets/toggle_down.png" height="15px"</span>
-		</button>
-		<a class="navbar-brand" href="memberprofile.php"><img src="assets/brand.png" height="45px" /></a>
-	      </div>
-		
-	      <!-- Collect the nav links, forms, and other content for toggling -->
-	      <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-		<ul class="nav navbar-nav">
-		  <li><a href="calendar/events.php">Events</a></li>
-		  <li><a href="neighbors.php">Neighbors</a></li>
-		  <li><a href="community.php">Community</a></li>
-		  <li>
-		    <div id="bulletinBox">
-		  		<div id="topBox">
-			    	<p id="appendTarget"style="color:white;">Bulletin Board <img src="../assets/right_arrow.png" height="12px" width="12px"></p>
-			    </div>	
-			    <div id="bottomBox">
-			        <p id="firstOne"><a href="../feed/bulletin.php">&nbsp;Updates</a></p>
-			        <p id="secondOne"><a href="../feed/bulletin.php">Promotions</a></p>
-			        <p id="thirdOne"><a href="../feed/bulletin.php">Events</a></p>
-			    </div>
-		    </div>
-		  </li>
-		</ul>
-		<p class="navbar-text navbar-right hidden-sm hidden-xs">changing social media</p>
-	      </div><!-- /.navbar-collapse -->
-	    </div><!-- /.container-fluid -->
-	</nav>
-	
-	<div class="row">
-	    <div id="toggleSide">
-		<div class="col-xs=2">
-		    <img src="assets/toggle_right.png" height="30px"/>
-		</div>
-		<div class="col-xs-10">
-		    <div id="sideNav side-Navigation" class="sNav">
-			<div id='sNav-inner'>
-			<p id='name' class='pushover'><?php echo $fbfullname; ?></p>
-			<img id='profpic' class='spaceUnder pushover' src="https://graph.facebook.com/<?php echo $user; ?>/picture?height=350&width=350">
-			<ul class='po'>
-			    <li class='spaceUnder'><a href='memberprofile.php'>Home</a></li>
-			    <li class='spaceUnder'><a href='#'>Messages</a></li>
-			    <li class='spaceUnder'><a href='#'>Settings</a></li>
-			    <li class='spaceUnder'><a href='chooseInterests.php'>Choose Interests</a></li>
-			    <li class='spaceUnder'><a href="<?php echo $logoutUrl; ?>">Logout</a></li>
-			</ul>
-		    </div>
-		    </div>
-		</div>
-	    </div><!-- end toggleSide -->
+    <?php include 'nav.php' ?>
 	    
 	    <div id='content' class='row'>
 		<div class="col-xs-10 col-sm-10 col-md-10 col-lg-10">
